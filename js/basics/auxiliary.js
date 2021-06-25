ddfc1dd45a2c3936da529e933c720a7d9eadaeae
pelliot <elliotkoh91@gmail.com>
2021-06-25 23:22:12 +0200

    Added splitting by class. Removed some unnecessary code.

--- js/basics/auxiliary.js
+++ js/basics/auxiliary.js
@@ -1,226 +1,4 @@
 /**
- * check if the given weights are valid
- * @returns {boolean}
- */
-function isValid() {
-    var valid = true;
-    if (document.getElementById("threshold").value.trim() === '' && document.getElementById("threshold").className === "show") {
-        alert("Please enter a threshold");
-        valid = false;
-    } else if (isNaN(document.getElementById("threshold").value)) {
-        alert("Invalid threshold");
-        valid = false;
-    }
-
-    if (document.getElementById("monotonicity").value.trim() === '' && document.getElementById("SGthresholds").className === "show") {
-        alert("Please enter a threshold for monotonicity");
-        valid = false;
-    } else if (isNaN(document.getElementById("monotonicity").value)) {
-        alert("Invalid threshold");
-        valid = false;
-    } else if (Number(document.getElementById("monotonicity").value) > 1 || Number(document.getElementById("monotonicity").value) < 0) {
-        alert("Threshold for monotonicity must be between 0 and 1");
-        valid = false;
-    } else if (document.getElementById("symmetry").value.trim() === '' && document.getElementById("SGthresholds").className === "show") {
-        alert("Please enter a threshold for symmetry");
-        valid = false;
-    } else if (isNaN(document.getElementById("symmetry").value)) {
-        alert("Invalid threshold");
-        valid = false;
-    } else if (Number(document.getElementById("symmetry").value) > 1 || Number(document.getElementById("symmetry").value) < 0) {
-        alert("Threshold for symmetry must be between 0 and 1");
-        valid = false;
-    }
-
-    if (document.getElementById("weights").className === "show") {
-        var currentSum = forDigit(Number(document.getElementById("nlc").value) + Number(document.getElementById("ac").value) +
-            Number(document.getElementById("para").value) + Number(document.getElementById("mi").value) +
-            Number(document.getElementById("condi").value) + Number(document.getElementById("op").value), 2);
-        if (document.getElementById("nlc").value.trim() === "") {
-            alert("Please give a weight to Number of Line Crossings");
-            valid = false;
-        } else if (isNaN(document.getElementById("nlc").value)) {
-            alert("Invalid input for Number of Line Crossings");
-            valid = false
-        } else if (Number(document.getElementById("nlc").value) > 1 || Number(document.getElementById("nlc").value) < 0) {
-            alert("Weight for Number of Line Crossings must be between 0 and 1");
-            valid = false;
-        }
-        else if (document.getElementById("ac").value.trim() === "") {
-            alert("Please give a weight to Angles of Crossing");
-            valid = false;
-        } else if (isNaN(document.getElementById("ac").value)) {
-            alert("Invalid input for Angles of Crossing");
-            valid = false
-        } else if (Number(document.getElementById("ac").value) > 1 || Number(document.getElementById("ac").value) < 0) {
-            alert("Weight for Angles of Crossing must be between 0 and 1");
-            valid = false;
-        }
-
-        else if (document.getElementById("para").value.trim() === "") {
-            alert("Please give a weight to Parallelism");
-            valid = false;
-        } else if (isNaN(document.getElementById("para").value)) {
-            alert("Invalid input for Parallelism");
-            valid = false
-        } else if (Number(document.getElementById("para").value) > 1 || Number(document.getElementById("para").value) < 0) {
-            alert("Weight for Parallelism must be between 0 and 1");
-            valid = false;
-        }
-        else if (document.getElementById("mi").value.trim() === "") {
-            alert("Please give a weight to Mutual Information");
-            valid = false;
-        } else if (isNaN(document.getElementById("mi").value)) {
-            alert("Invalid input for Mutual Information");
-            valid = false
-        } else if (Number(document.getElementById("mi").value) > 1 || Number(document.getElementById("mi").value) < 0) {
-            alert("Weight for Mutual Information must be between 0 and 1");
-            valid = false;
-        }
-        else if (document.getElementById("condi").value.trim() === "") {
-            alert("Please give a weight to Convergence & Divergence");
-            valid = false;
-        } else if (isNaN(document.getElementById("condi").value)) {
-            alert("Invalid input for Convergence & Divergence");
-            valid = false
-        } else if (Number(document.getElementById("condi").value) > 1 || Number(document.getElementById("condi").value) < 0) {
-            alert("Weight for Convergence & Divergence must be between 0 and 1");
-            valid = false;
-        }
-        else if (document.getElementById("op").value.trim() === "") {
-            alert("Please give a weight to Over-plotting");
-            valid = false;
-        } else if (isNaN(document.getElementById("op").value)) {
-            alert("Invalid input for Over-plotting");
-            valid = false
-        } else if (Number(document.getElementById("op").value) > 1 || Number(document.getElementById("op").value) < 0) {
-            alert("Weight for Over-plotting must be between 0 and 1");
-            valid = false;
-        } else if (Number(currentSum.toFixed(2)) !== 1) {
-            valid = false;
-            alert("Sum of all weights must be 1(current sum: )" + currentSum);
-        }
-    }
-    return valid;
-}
-
-/**
- *  show the further options needed for a specific ordering method
- */
-function showSim() {
-    var selectedMethod = document.getElementById("list").value;
-    switch (selectedMethod) {
-        case "0":
-            document.getElementById("similarity").className = "hidden";
-            document.getElementById("choose").innerHTML = "";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*Data sets are displayed according to the original axes order.";
-            break;
-        case "1":
-            document.getElementById("similarity").className = "hidden";
-            document.getElementById("choose").innerHTML = "Please give a threshold for cluster distance:";
-            document.getElementById("threshold").className = "show";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This method orders the axes so that the number of outliers between two dimensions are minimized.<br>*Reference: <a href=\"https://ieeexplore.ieee.org/abstract/document/1382895/\">Clutter Reduction in Multi-Dimensional Data Visualization Using Dimension Reordering</a>";
-            break;
-        case "2":
-            document.getElementById("similarity").className = "hidden";
-            document.getElementById("choose").innerHTML = "";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This method orders axes according to their contribution rates, which is determined by Singular Value Decomposition. <br>*Reference: <a href=\"https://www.sciencedirect.com/science/article/pii/S1045926X15300379\">Two axes re-ordering methods in parallel coordinates plots</a>";
-            break;
-        case "3":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This methods uses the Nearest Neighbor Heuristic and arranges similar axes as close as possible to each other. <br>*Reference: <a href = \"https://ieeexplore.ieee.org/abstract/document/1648337/\">Enhanced High Dimensional Data Visualization through Dimension Reduction and Attribute Arrangement</a>";
-            break;
-        case "4":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This method puts the dimension with the highest contribution rate at the left-most position, and add the most similar axes afterwards. <br> *Reference: <a href=\"https://www.sciencedirect.com/science/article/pii/S1045926X15300379\">Two axes re-ordering methods in parallel coordinates plots</a>";
-            break;
-        case "5":
-            document.getElementById("similarity").className = "hidden";
-            document.getElementById("choose").innerHTML = "";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This method investigates the slope of each data points in the pattern and returns the order that results in a minimized pattern.<br>*Reference: <a href=\"https://www.ijcaonline.org/archives/volume40/number13/5044-7370\">Axes Re-Ordering in Parallel Coordinate for Pattern Optimization</a>";
-            break;
-        case "6":
-            document.getElementById("choose").innerHTML = "Please define weights between 0 and 1 for each metrics: (All weights should sum up to 1)";
-            document.getElementById("similarity").className = "hidden";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "show";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This method arranges axes based on the flowing quality metrics for parallel coordinates and user-defined weights. *Reference: <a href=\"https://ieeexplore.ieee.org/document/5613439/\">Pargnostics: Screen-Space Metrics for Parallel Coordinates</a>";
-            break;
-        case "7":
-            document.getElementById("choose").innerHTML = "Please choose the thresholds for monotonicity and symmetry:";
-            document.getElementById("similarity").className = "hidden";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "show";
-            document.getElementById("basicInfo2").innerHTML = "*This method minimize the total occurrence of unstructured rays in glyphs, where clutter is defined as the total number of non-monotonic and non-symmetric occurrences. *Reference: <a href=\"http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.308.2531\">Pargnostics: Screen-Space Metrics for Parallel Coordinates</a>";
-            break;
-        case "8":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This methods arranges the dimensions so that neighboring sequence has the overall highest similarity";
-            break;
-        case "9":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This methods arranges the dimensions so that neighboring sequence has the overall lowest similarity";
-            break;
-        case "10":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This methods arranges the dimensions so that neighboring sequence has the overall lowest similarity";
-            break;
-        case "11":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This methods arranges the dimensions so that neighboring sequence has the overall lowest similarity";
-            break;
-        case "12":
-            document.getElementById("choose").innerHTML = "Please choose a similarity measure:";
-            document.getElementById("similarity").className = "show";
-            document.getElementById("threshold").className = "hidden";
-            document.getElementById("weights").className = "hidden";
-            document.getElementById("SGthresholds").className = "hidden";
-            document.getElementById("basicInfo2").innerHTML = "*This methods uses the Nearest Neighbor Heuristic and arranges dissimilar axes as close as possible to each other. <br>*Reference: <a href = \"https://ieeexplore.ieee.org/abstract/document/1648337/\">Enhanced High Dimensional Data Visualization through Dimension Reduction and Attribute Arrangement</a>";
-            break;
-        default:
-            alert("Sorry, something is wrong.");
-
-    }
-}
-
-/**
  * Disable Button
  * @param obj
  */
@@ -617,7 +395,7 @@ function permutator(inputArr) {
  * @param dataArray
  * @returns {Array}
  */
-function getNomArr(dataArray) {
+function getNormalizedArr(dataArray) {
     var dataMat = getDataMat(dataArray);
     var nomArr = deepCopy(dataArray);
     for (var dim = 0; dim < dataMat[0].length; dim++) {
@@ -635,21 +413,44 @@ function getNomArr(dataArray) {
 /**
  * Convert an array to array of objects
  * @param data
+ * @param classes
  * @returns {Array}
  */
-function convertToArrayOfObjects(data) {
+function convertToArrayOfObjects(data, classes) {
     var keys = data.shift(),
         i = 0, k = 0,
-        obj = null,
-        output = [];
-    for (i = 0; i < data.length; i++) {
-        obj = {};
-        for (k = 0; k < keys.length; k++) {
-            obj[keys[k]] = data[i][k];
-        }
-        output.push(obj);
+        classDict = {},
+        rowDict = null;
+
+    for (i = 0; i < classes.length; i++){
+        classDict[classes[i]] = [];
     }
-    return output;
+
+    for (i = 0; i < data.length; i++) {
+        rowDict = {};
+        rowClass = data[i][0]
+        for (k = 1; k < keys.length; k++) {
+            rowDict[keys[k]] = data[i][k];
+        }
+        classDict[rowClass].push(rowDict);
+    }
+    return classDict;
+}
+
+/**
+ * Get a list of classes in the data
+ * @param data
+ * @returns {Array}
+ */
+function getClasses(data) {
+    var classes = [];
+
+    for (i = 1; i < data.length; i++) {
+        if (!classes.includes(data[i][0])){
+            classes.push(data[i][0]);
+        }
+    }
+    return classes;
 }
 
 /**
@@ -831,7 +632,7 @@ function getSimValues(simMat) {
 }
 
 function standardDeviation(numbersArr) {
-    //--CALCULATE AVAREGE--
+    //--CALCULATE AVERAGE--
     var total = 0;
     for (var key in numbersArr)
         total += numbersArr[key];
--- js/visualization/pcVis.js
+++ js/visualization/pcVis.js
@@ -3,8 +3,8 @@
  * link: https://bl.ocks.org/mbostock/7586334
  */
 
-function pcVis(dataset) {
-    var m = [30, 10, 10, 10],
+function pcVis(dataDict, classes) {
+    var m = [30, 10, 10, 10], //margin
         w = 960 - m[1] - m[3],
         h = 500 - m[0] - m[2];
 
@@ -17,107 +17,105 @@ function pcVis(dataset) {
         background,
         foreground;
 
-
     document.getElementById("targetPC").className = "show";
-    // Extract the list of dimensions and create a scale for each.
-    x.domain(dimensions = d3.keys(dataset[0]).filter(function (d) {
-        if (document.getElementById("normalized").checked) {
-            return d !== "name" && (y[d] = d3.scale.linear()
-            //.domain(d3.extent(dataset, function (p) {
-            //  return +p[d];
-            // }))
-                .range([h, 0]));
-        } else {
-            return d !== "name" && (y[d] = d3.scale.linear()
+
+    for (i=0; i<classes.length; i++){
+        c = classes[i];
+        var dataset = dataDict[c];
+        
+        console.log(dataset);
+        // Extract the list of dimensions and create a scale for each.
+        x.domain(dimensions = d3.keys(dataset[0]).filter(function (d) {
+            // return d !== "name" && (y[d] = d3.scale.linear()
+            return (y[d] = d3.scale.linear()
                 .domain(d3.extent(dataset, function (p) {
                     return +p[d];
                 }))
                 .range([h, 0]));
-        }
-    }));
+        }));
 
-    var svg = d3.select("#targetPC").append("svg")
-        .attr("width", w + m[1] + m[3])
-        .attr("height", h + m[0] + m[2])
-        .append("g")
-        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+        var svg = d3.select("#targetPC").append("svg")
+            .attr("width", w + m[1] + m[3])
+            .attr("height", h + m[0] + m[2])
+            .append("g")
+            .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
 
-    // Add grey background lines for context.
-    background = svg.append("g")
-        .attr("class", "background")
-        .selectAll("path")
-        .data(dataset)
-        .enter().append("path")
-        .attr("d", path);
+        // Add grey background lines for context.
+        background = svg.append("g")
+            .attr("class", "background")
+            .selectAll("path")
+            .data(dataset)
+            .enter().append("path")
+            .attr("d", path);
 
-    // Add blue foreground lines for focus.
-    foreground = svg.append("g")
-        .attr("class", "foreground")
-        .selectAll("path")
-        .data(dataset)
-        .enter().append("path")
-        .attr("d", path);
+        // Add blue foreground lines for focus.
+        foreground = svg.append("g")
+            .attr("class", "foreground")
+            .selectAll("path")
+            .data(dataset)
+            .enter().append("path")
+            .attr("d", path);
 
-    // Add a group element for each dimension.
-    var g = svg.selectAll(".dimension")
-        .data(dimensions)
-        .enter().append("g")
-        .attr("class", "dimension")
-        .attr("transform", function (d) {
-            return "translate(" + x(d) + ")";
-        })
-        .call(d3.behavior.drag()
-            .on("dragstart", function (d) {
-                dragging[d] = this.__origin__ = x(d);
-                background.attr("visibility", "hidden");
+        // Add a group element for each dimension.
+        var g = svg.selectAll(".dimension")
+            .data(dimensions)
+            .enter().append("g")
+            .attr("class", "dimension")
+            .attr("transform", function (d) {
+                return "translate(" + x(d) + ")";
             })
-            .on("drag", function (d) {
-                dragging[d] = Math.min(w, Math.max(0, this.__origin__ += d3.event.dx));
-                foreground.attr("d", path);
-                dimensions.sort(function (a, b) {
-                    return position(a) - position(b);
-                });
-                x.domain(dimensions);
-                g.attr("transform", function (d) {
-                    return "translate(" + position(d) + ")";
+            .call(d3.behavior.drag()
+                .on("dragstart", function (d) {
+                    dragging[d] = this.__origin__ = x(d);
+                    background.attr("visibility", "hidden");
                 })
+                .on("drag", function (d) {
+                    dragging[d] = Math.min(w, Math.max(0, this.__origin__ += d3.event.dx));
+                    foreground.attr("d", path);
+                    dimensions.sort(function (a, b) {
+                        return position(a) - position(b);
+                    });
+                    x.domain(dimensions);
+                    g.attr("transform", function (d) {
+                        return "translate(" + position(d) + ")";
+                    })
+                })
+                .on("dragend", function (d) {
+                    delete this.__origin__;
+                    delete dragging[d];
+                    transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                    transition(foreground)
+                        .attr("d", path);
+                    background
+                        .attr("d", path)
+                        .transition()
+                        .delay(500)
+                        .duration(0)
+                        .attr("visibility", null);
+                }));
+
+        // Add an axis and title.
+        g.append("g")
+            .attr("class", "axis")
+            .each(function (d) {
+                d3.select(this).call(axis.scale(y[d]));
             })
-            .on("dragend", function (d) {
-                delete this.__origin__;
-                delete dragging[d];
-                transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-                transition(foreground)
-                    .attr("d", path);
-                background
-                    .attr("d", path)
-                    .transition()
-                    .delay(500)
-                    .duration(0)
-                    .attr("visibility", null);
-            }));
+            .append("text")
+            .attr("text-anchor", "middle")
+            .attr("y", -9)
+            .text(String);
 
-    // Add an axis and title.
-    g.append("g")
-        .attr("class", "axis")
-        .each(function (d) {
-            d3.select(this).call(axis.scale(y[d]));
-        })
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("y", -9)
-        .text(String);
-
-    // Add and store a brush for each axis.
-    g.append("g")
-        .attr("class", "brush")
-        .each(function (d) {
-            d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
-        })
-        .selectAll("rect")
-        .attr("x", -8)
-        .attr("width", 16);
-
+        // Add and store a brush for each axis.
+        g.append("g")
+            .attr("class", "brush")
+            .each(function (d) {
+                d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
+            })
+            .selectAll("rect")
+            .attr("x", -8)
+            .attr("width", 16);
+    }
 
     function position(d) {
         var v = dragging[d];
