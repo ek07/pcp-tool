3bee84533ee7825f5f22ea5edbaf42318e030153
pelliot <elliotkoh91@gmail.com>
2021-07-11 12:55:29 +0200

    added regression plots. also modified layout of table slightly.

--- js/table.js
+++ js/table.js
@@ -81,6 +81,8 @@ function computeTable(results){
     // Example of how to use perm gen //
     var done = false;
     var id = 1;
+    var corr_scatter_vals = [];
+    var polydist_scatter_vals = [];
     while (!done){
         var perm = ordering_generator.next()
         var ordering = perm.value
@@ -94,30 +96,37 @@ function computeTable(results){
             table_row["ordering"] = ordering;
 
             var qfd = getQFD(featureVector, dimDistMatrix, classDict, dataArray.length, ordering, dist_type);
-            table_row["qfd"] = qfd.toFixed(3);
+            table_row["qfd"] = qfd.toFixed(4);
             
             var corr = getCorrFromCorrMats(corrMats, classes, ordering);
-            table_row["correlation"] = corr.toFixed(3);
+            table_row["correlation"] = corr.toFixed(4);
+            corr_scatter_vals.push({'x':qfd, 'y':corr});
 
             var mean_line_length = computeDistFromMat(plcMat, ordering);
-            table_row["linedist"] = mean_line_length.toFixed(3);
+            table_row["linedist"] = mean_line_length.toFixed(4);
+            polydist_scatter_vals.push({'x':qfd, 'y':mean_line_length});
 
             table_values.push(table_row);
         }
     }
 
     createTable(table_values);
+    plotScatter(corr_scatter_vals, "#corr_scatter", "#0000FF", "Mean correlation"); // blue
+    plotScatter(polydist_scatter_vals, "#poly_scatter", "#FF0000", "Mean polyline distance"); // red
 }
 
 function createTable(tableData){
+    var screen_height = screen.height/2;
     var table = new Tabulator("#example-table", {
         // layout:"fitDataFill",
-        // layout:"fitDataTable",
-        layout:"fitColumns",
-        height:"511px",
+        layout:"fitDataTable",
+        // layout:"fitDataStretch",
+        // layout:"fitColumns",
+        height:screen_height+"px",
 
     columns:[
-    {title:"Ordering", field:"ordering", sorter:"number", frozen:true}, // frozen:true
+    {title:"Index", field:"id", sorter: "number", frozen:true},
+    {title:"Ordering", field:"ordering", sorter:"string"}, // frozen:true
     {title:"Total OFD", field:"qfd", sorter:"number"},
     {title:"Mean correlation", field:"correlation", sorter:"number"},
     {title:"Mean polyline distance", field:"linedist", sorter:"number"},
@@ -128,4 +137,123 @@ function createTable(tableData){
     //                 {id:2, "ordering":[0,1,3,2], qfd:45, correlation:12.3, linedist:0.62}], true);   
 
     table.addData(tableData, true);
+}
+
+
+function plotScatter(data, fig_id, scatter_color, y_label){
+    d3.select(fig_id).selectAll("svg").remove();
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 40, bottom: 45, left: 60},
+        // width = 460 - margin.left - margin.right,
+        // height = 400 - margin.top - margin.bottom;
+        width = screen.width/3 - margin.left - margin.right,
+        height = screen.height/2 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(fig_id)
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // Add X axis
+    var x_dom = d3.extent(data, function (d) {
+                    return d.x; // the plus converts string to number
+                })
+    var x_scale = (x_dom[1]-x_dom[0])*0.1;
+    x_dom = [x_dom[0]-x_scale, x_dom[1]+x_scale];
+
+    var x = d3.scaleLinear()
+    .domain(x_dom)
+    .range([ 0, width ]);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y_dom = d3.extent(data, function (d) {
+                return d.y; // the plus converts string to number
+            })
+    var y_scale = (y_dom[1]-y_dom[0])*0.1;
+    y_dom = [y_dom[0]-y_scale, y_dom[1]+y_scale];
+
+    var y = d3.scaleLinear()
+    .domain(y_dom)
+    .range([ height, 0]);
+    svg.append("g")
+    .call(d3.axisLeft(y));
+
+    // Add dots
+    svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.x); } )
+      .attr("cy", function (d) { return y(d.y); } )
+      .attr("r", 2)
+      .style("fill", scatter_color)
+
+    // Plot regression line
+    var linearRegression = d3.regressionLinear()
+   .x(d => d.x)
+   .y(d => d.y);
+
+    var res = linearRegression(data)
+    let line = d3.line()
+      .x((d) => x(d[0]))
+      .y((d) => y(d[1]));
+
+    svg.append("path")
+      .datum(res)
+      .attr("d", line)
+      .style("stroke", "black")
+      .style("stroke-width", "2.5px");
+
+    //Create Title 
+    svg.append("text")
+    .attr("x", width / 2 )
+    .attr("y", 0)
+    .style("text-anchor", "middle")
+    .text(y_label + " vs QFD");
+
+ // Add X axis label:
+  svg.append("text")
+      .attr("text-anchor", "end")
+      .attr("x", width/2 + margin.left -20)
+      .attr("y", height + margin.top + 20)
+      .text("QFD");
+
+  // Y axis label:
+  svg.append("text")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left + 20)
+      .attr("x", -margin.top - height/2 + 90)
+      .text(y_label)
+}
+
+
+function saveAsSVG() {
+    var svgString = getSVGString();
+    var svg_text = new Blob([svgString],
+        { type: "image/svg+xml;charset=utf-8" }); // type:"text/svg;charset=utf-8"
+    saveAs(svg_text, "test.svg");
+
+}
+
+// https://stackoverflow.com/questions/23218174/how-do-i-save-export-an-svg-file-after-creating-an-svg-with-d3-js-ie-safari-an
+function getSVGString(){
+    const xmlns = "http://www.w3.org/2000/xmlns/";
+    const xlinkns = "http://www.w3.org/1999/xlink";
+    const svgns = "http://www.w3.org/2000/svg";
+    // var svgString = d3.select("#stringcharter").node().innerHTML; // inner or outer html? svg
+    var svgString = document.getElementById("contentDiv").innerHTML;
+
+    //add name spaces.
+    svgString = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" >" + svgString + "</svg>";
+    return svgString;
 }
